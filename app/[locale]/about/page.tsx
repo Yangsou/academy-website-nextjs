c54e621d1b2978bf5a9800bd4ffaeb1f25216c9c
Nguyen Trung Nam <nam.nguyen@aidi.world>
2026-01-13 13:42:49 +0700

    feat: update feedback 2

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -4,10 +4,11 @@ import OurTeam from '@/components/our-team'
 
 export default function AboutPage() {
   return (
-    <>
+    <div className="relative">
       <AboutBanner />
       <CoreValues />
       <OurTeam />
-    </>
+      <div className="relative h-[104px] w-full bg-[#DAF3F4] bg-[url('/about/footer-bg.png')] bg-cover bg-bottom bg-no-repeat sm:h-[208px] lg:h-[280px] lg:bg-cover xl:h-[382px] 2xl:h-[504px]" />
+    </div>
   )
 }
