3df987160708d105ddc43789f72b3ae32881a3f5
nam.nguyen <nam.nguyen@redex.eco>
2026-01-30 21:55:18 +0700

    ui

--- components/pdt-academy/pdt-academy-statistic.tsx
+++ components/pdt-academy/pdt-academy-statistic.tsx
@@ -73,7 +73,7 @@ export default function PdtAcademyStatistic() {
                 <Image
                   src="/product/pdt-aca-ai-image-01.png"
                   fill
-                  className="object-cover"
+                  className="rounded-[20px] object-cover"
                   alt=""
                 />
               </AspectRatio>
